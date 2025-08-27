e41eb3a16bb066924e9c2894e4a83585927d000e
George Lugo <machinelearner2334@gmail.com>
2025-08-27 01:11:29 +0000

    fix: made proper changes

--- liqui/vite.config.ts
+++ liqui/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: 5173,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
