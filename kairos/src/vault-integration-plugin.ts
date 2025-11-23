0dbb64b9c53a1e5e47d34d6f8d0bd91931696ca1
George Lugo <machinelearner2334@gmail.com>
2025-11-23 02:58:28 +0000

    fix: made proper changes

--- kairos/src/vault-integration-plugin.ts
+++ kairos/src/vault-integration-plugin.ts
@@ -1,5 +1,5 @@
 import { Plugin } from '@elizaos/core';
-import SEIVaultManager from './services/sei-vault-manager';
+// import SEIVaultManager from './services/sei-vault-manager'; // Temporarily disabled
 import vaultMonitorEvaluator, {
   rebalancingEvaluator,
   healthCheckEvaluator,
@@ -20,7 +20,7 @@ import vaultMonitorEvaluator, {
 export const vaultIntegrationPlugin: Plugin = {
   name: 'vault-integration',
   description: 'SEI Vault integration for automated yield generation',
-  services: [SEIVaultManager],
+  services: [], // Temporarily disabled - will use evaluators instead
   evaluators: [vaultMonitorEvaluator, rebalancingEvaluator, healthCheckEvaluator],
   actions: [],
 };
